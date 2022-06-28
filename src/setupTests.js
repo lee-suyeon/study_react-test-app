b9a14ebe4d1afa1add95a47969b43deb5ed5ebb8
suyeon <sroovy@naver.com>
2022-06-28 22:01:05 +0900

    Mock Service Worker

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// before test
+beforeAll(() => server.listen());
+
+// 다른 테스트에 영향X
+afterEach(() => server.resetHandlers());
+
+// 테스트 종료 후 서버 clean up
+afterEach(() => server.close());
