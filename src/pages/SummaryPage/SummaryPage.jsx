bafbf8d5e4534691b473995ca7be85bb1fb20594
suyeon <sroovy@naver.com>
2022-06-28 22:00:14 +0900

    SummaryPage bug fix

--- src/pages/SummaryPage/SummaryPage.jsx
+++ src/pages/SummaryPage/SummaryPage.jsx
@@ -16,10 +16,10 @@ function SummaryPage() {
           onChange={onChangeHandler}
           id="confirm-checkbox"
         />
-        <label htmlFor="confirm-checkbox">"주문하려는 것을 확인하셨나요?"</label>
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
         <br />
         <button disabled={!checked} type="submit">
-          주문확인
+          주문 확인
         </button>
       </form>
     </div>
