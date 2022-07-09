7dbd9dbca7bd15536c37e9690e5c13367dbf8b76
suyeon <sroovy@naver.com>
2022-07-09 22:40:52 +0900

    상품가격, 옵션 가격을 더한 총 가격 구하기

--- src/pages/OrderPage/tests/calculate.test.js
+++ src/pages/OrderPage/tests/calculate.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '../../../test-utils'
 import userEvent from "@testing-library/user-event";
 import Type from "../Type";
+import OrderPage from '../OrderPage';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
@@ -39,4 +41,54 @@ test("update option's total when options change", async () => {
 
   userEvent.click(dinnerCheckbox);
   expect(optionsTotal).toHaveTextContent("500");
+});
+
+describe("total price of goods and options", () => {
+  test("total price starts with 0 and Updating total price when adding one product", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    expect(total).toHaveTextContent("0");
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    })
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    expect(total).toHaveTextContent("1000");
+  });
+
+  test("Updating total price when adding one option", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance"
+    })
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("500");
+  });
+
+  test("Updating total price when removing option and product", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance"
+    });
+    userEvent.click(insuranceCheckbox);
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "3");
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    expect(total).toHaveTextContent("1500")
+  });
 })
