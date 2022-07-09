f55dcbdc830523cb8bbc7893893bf4da117c716b
suyeon <sroovy@naver.com>
2022-07-09 22:24:06 +0900

    옵션 가격을 위한 테스트 구현

--- src/pages/OrderPage/tests/calculate.test.js
+++ src/pages/OrderPage/tests/calculate.test.js
@@ -5,7 +5,7 @@ import Type from "../Type";
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
 
-  const productsTotal = screen.getByText("총 가격", { exact: false });
+  const productsTotal = screen.getByText("상품 총 가격", { exact: false });
   expect(productsTotal).toHaveTextContent("0");
 
   // 아메리카 상품 한개 올리기
@@ -18,3 +18,25 @@ test("update product's total when products change", async () => {
   expect(productsTotal).toHaveTextContent("1000");
 })
 
+test("update option's total when options change", async () => {
+  render(<Type orderType="options"/>)
+
+  const optionsTotal = screen.getByText("옵션 총 가격", { exact: false });
+  expect(optionsTotal).toHaveTextContent("0");
+
+  const insuranceCheckbox = await screen.findByRole('checkbox', {
+    name: "Insurance",
+  });
+
+  userEvent.click(insuranceCheckbox);
+  expect(optionsTotal).toHaveTextContent("500");
+
+  const dinnerCheckbox = await screen.findByRole('checkbox', {
+    name: "Dinner",
+  })
+  userEvent.click(dinnerCheckbox);
+  expect(optionsTotal).toHaveTextContent("1000");
+
+  userEvent.click(dinnerCheckbox);
+  expect(optionsTotal).toHaveTextContent("500");
+})
