961f56de986f712111c7c2dd397b88b3067ec920
suyeon <sroovy@naver.com>
2022-07-11 22:41:12 +0900

    주문 완료 페이지

--- src/pages/SummaryPage/SummaryPage.jsx
+++ src/pages/SummaryPage/SummaryPage.jsx
@@ -1,15 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { OrderContext } from '../../contexts/OrderContext'
 
-function SummaryPage() {
+function SummaryPage({ setStep }) {
   const [ checked, setChecked ] = useState(false);
+  const [ orderDatas ] = useContext(OrderContext);
 
+  const productArray = Array.from(orderDatas.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = orderDatas.options.size > 0;
+  let optionsRender = null;
+  if(hasOptions) {
+    const optionsArray = Array.from(orderDatas.options.keys());
+    const optionList = optionsArray.map(key => <li key={key}>{key}</li>)
+    optionsRender = (
+      <>
+        <h2>옵션: {orderDatas.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </>
+    )
+  }
+  
   const onChangeHandler = e => {
     setChecked(e.taget.checked);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2)
+  }
+
   return (
     <div>
-      <form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품: {orderDatas.totals.products}</h2>
+      <ul>{productList}</ul>
+      {optionsRender}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           checked={checked}
