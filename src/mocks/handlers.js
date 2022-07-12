87ddde5683499e22a6518ca0f3a84c4a465c51ba
suyeon <sroovy@naver.com>
2022-07-12 21:59:42 +0900

    주문 완료 페이지

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -26,5 +26,9 @@ export const handlers = [
         }
       ])
     )
+  }),
+  rest.post("http://localhost:5000/order", (req, res, ctx) => {
+    let dummyData = [{ orderNumber: 2131313, price: 2000 }];
+    return res(ctx.json(dummyData));
   })
 ]
---
+++ src/pages/CompletePage/CompletePage.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useContext } from 'react'
+import axios from 'axios';
+import { OrderContext } from '../../contexts/OrderContext';
+import ErrorBanner from '../../components/ErrorBanner';
+
+function CompletePage({ setStep }) {
+  const [ OrderDatas ] = useContext(OrderContext);
+  const [ orderHistory, setOrderHistory ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(false);
+
+  useEffect(() => {
+    orderCompleted(OrderDatas);
+  }, [input]);
+
+  const orderCompleted = async (OrderDatas) => {
+    try {
+      let response = await axios.post("http://localhost:5000/order", OrderDatas);
+      setOrderHistory(response.data);
+      setLoading(false);
+    } catch(error) {
+      setError(true);
+    }
+  }
+
+  if(error) {
+    return <ErrorBanner message="에러가 발생했습니다." />
+  }
+
+  const orderTable = orderHistory.map(item => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ))
+
+  if(loading) {
+    return <div>loading</div>
+  } else {
+    return <div style={{ textAlign: "center" }}>
+      <h2>주문이 성공했습니다.</h2>
+      <h3>지금까지 모든 주문</h3>
+      <table style={{ margin: "auto"}}>
+        <tbody>
+          <tr>
+            <th>주문 번호</th>
+            <th>주문 가격</th>
+          </tr>
+          {orderTable}
+        </tbody>
+      </table>
+      <br />
+      <button onClick={() => setStep(0)}>첫페이지로</button>
+    </div>
+  }
+
+}
+
+export default CompletePage
